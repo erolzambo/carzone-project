df0cd64b34654f9671da477108fddc2b2c169390
Erol Bilican <erol.bilican@gmail.com>
2021-06-10 21:57:35 +0800

    pages created and links are configured

--- carzone/settings.py
+++ carzone/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'pages',
+    # 'pages',
     'pages.apps.PagesConfig',
 ]
 
@@ -114,3 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'carzone/static'),
+]
